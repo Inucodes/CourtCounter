96ece95b3213acf7a269325efc32ee064f57e556
Marcin Dolegowski <marcindolegowski84@gmail.com>
2018-05-15 11:43:18 +0200

    Initial commit

---
+++ app/src/main/java/com/example/inu/courtcounter/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.inu.courtcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+// Calculations for TEAM B
+
+    public void addThreeForTeamA(View v) {
+
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
+
+
+    }
+
+    public void addTwoForTeamA(View v) {
+
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
+
+
+    }
+
+
+    public void addOneForTeamA(View v) {
+
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
+
+
+    }
+
+
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+
+
+    }
+
+// Calculations for TEAM B
+
+
+    public void addThreeForTeamB(View v) {
+
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+
+
+    }
+
+    public void addTwoForTeamB(View v) {
+
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+
+
+    }
+
+
+    public void addOneForTeamB(View v) {
+
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+
+
+    }
+
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+
+
+    }
+
+
+    public void ResetPoints(View v) {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+
+
+    }
+
+
+}
